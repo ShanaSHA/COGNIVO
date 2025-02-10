d56ff3fb8c2ceeec9c2286df2ca09c56291b823f
Shana Yasmin <121674890+ShanaSHA@users.noreply.github.com>
2025-02-10 09:59:19 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="bg-black p-4 text-white flex justify-between">
       <h1 className="text-xl font-bold">Study Planner</h1>
       <div>
-        <Link to="/" className="mr-4">Dashboard</Link>
+        <Link to="/dashboard" className="mr-4">Dashboard</Link>
         <Link to="/sessions" className="mr-4">Sessions</Link>
         <Link to="/calendar">Calendar</Link>
         <Link to="/" className="px-4">LogOut</Link>
